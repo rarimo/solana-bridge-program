2e238613781e3a7592a78976807a116dbb5f1663
olegfomenko2002 <oleg.fomenko2002@gmail.com>
2023-03-31 15:41:10 +0300

    adding seed const string to import in commission contracts

--- src/commission.rs
+++ src/commission.rs
@@ -2,6 +2,8 @@ use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::pubkey::Pubkey;
 use crate::state::TokenType;
 
+pub const COMMISSION_ADMIN_PDA_SEED: &str = "commission_admin";
+
 #[repr(C)]
 #[derive(BorshSerialize, BorshDeserialize, PartialEq, Debug, Clone)]
 pub enum CommissionToken {
